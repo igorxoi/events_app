cf809987b4e09cdc986b4ad124cbef89fa8b2674
Felipe Silva <felipe.ferreira1003@gmail.com>
2025-11-05 22:16:22 -0300

    feat: include google maps api and component to direction

--- lib/pages/event_details.dart
+++ lib/pages/event_details.dart
@@ -1,4 +1,5 @@
 import 'package:events_app/models/event.dart';
+import 'package:events_app/components/location_map.dart';
 import 'package:events_app/services/favorite_handler.dart';
 import 'package:events_app/services/share.dart';
 import 'package:events_app/themes/colors.dart';
@@ -75,185 +76,194 @@ class _EventDetailsPageState extends State<EventDetailsPage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        "https://f.i.uol.com.br/fotografia/2024/02/08/170741929465c5269eeecc5_1707419294_3x2_md.jpg",
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 300,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Colors.black.withOpacity(0.7),
-                      ],
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    top: 230.0,
-                    left: 32.0,
-                    right: 32.0,
-                    bottom: 16.0,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
                     children: [
                       Container(
-                        padding: EdgeInsets.all(12.0),
-                        decoration: BoxDecoration(
-                          color: primaryDarkColor,
-                          borderRadius: BorderRadius.circular(8),
+                        height: 300,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              "https://f.i.uol.com.br/fotografia/2024/02/08/170741929465c5269eeecc5_1707419294_3x2_md.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        alignment: Alignment.center,
+                      ),
+                      Container(
+                        height: 300,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.transparent,
+                              Colors.black.withOpacity(0.7),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 230.0,
+                          left: 32.0,
+                          right: 32.0,
+                          bottom: 16.0,
+                        ),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              widget.event.name,
-                              style: const TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: whiteColor,
+                            Container(
+                              padding: EdgeInsets.all(12.0),
+                              decoration: BoxDecoration(
+                                color: primaryDarkColor,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              alignment: Alignment.center,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    widget.event.name,
+                                    style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: whiteColor,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.calendar_today_outlined,
+                                        size: 16,
+                                        color: whiteColor,
+                                      ),
+                                      const SizedBox(width: 4),
+                                      Text(
+                                        formatDate(widget.event.date),
+                                        style: const TextStyle(
+                                          color: whiteColor,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 16),
+                                      const Icon(
+                                        Icons.access_time_outlined,
+                                        size: 16,
+                                        color: whiteColor,
+                                      ),
+                                      const SizedBox(width: 4),
+                                      Text(
+                                        widget.event.startTime,
+                                        style: const TextStyle(
+                                          color: whiteColor,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.location_on_outlined,
+                                        size: 16,
+                                        color: whiteColor,
+                                      ),
+                                      const SizedBox(width: 4),
+                                      Text(
+                                        '${widget.event.address.city} - ${widget.event.address.state}',
+                                        style: const TextStyle(
+                                          color: whiteColor,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
                             ),
-                            const SizedBox(height: 8),
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.calendar_today_outlined,
-                                  size: 16,
-                                  color: whiteColor,
-                                ),
-                                const SizedBox(width: 4),
-                                Text(
-                                  formatDate(widget.event.date),
-                                  style: const TextStyle(
-                                    color: whiteColor,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                const SizedBox(width: 16),
-                                const Icon(
-                                  Icons.access_time_outlined,
-                                  size: 16,
-                                  color: whiteColor,
-                                ),
-                                const SizedBox(width: 4),
-                                Text(
-                                  widget.event.startTime,
-                                  style: const TextStyle(
-                                    color: whiteColor,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
+                            const SizedBox(height: 16),
+                            const Text(
+                              'Local',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: blackColor,
+                              ),
+                            ),
+                            const SizedBox(height: 12),
+                            Text(
+                              '${widget.event.address.street}, ${widget.event.address.number}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: blackColor,
+                              ),
                             ),
                             const SizedBox(height: 4),
-                            Row(
-                              children: [
-                                const Icon(
-                                  Icons.location_on_outlined,
-                                  size: 16,
-                                  color: whiteColor,
-                                ),
-                                const SizedBox(width: 4),
-                                Text(
-                                  '${widget.event.address.city} - ${widget.event.address.state}',
-                                  style: const TextStyle(
-                                    color: whiteColor,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
+                            Text(
+                              '${widget.event.address.neighborhood}, ${widget.event.address.city} - ${widget.event.address.state}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: blackColor,
+                              ),
                             ),
+                            const SizedBox(height: 12),
+                            LocationMap(
+                              eventLatitude: widget.event.address.latitude,
+                              eventLongitude: widget.event.address.longitude,
+                              height: 200,
+                            ),
+                            const SizedBox(height: 24),
                           ],
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      const Text(
-                        'Local',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: blackColor,
-                        ),
-                      ),
-                      const SizedBox(height: 12),
-                      Text(
-                        '${widget.event.address.street}, ${widget.event.address.number}\n${widget.event.address.neighborhood}, ${widget.event.address.city} - ${widget.event.address.state}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: textColorPrimary,
-                          height: 1.5,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Container(
-                        height: 200,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const Center(
-                          child: Text(
-                            'Mapa será adicionado aqui',
-                            style: TextStyle(color: textColorPrimary),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 24),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            ShareService().shareEvent(
-                              context: context,
-                              event: widget.event,
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          child: const Text(
-                            'Compartilhar',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: whiteColor,
-                            ),
-                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            child: SafeArea(
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    ShareService().shareEvent(
+                      context: context,
+                      event: widget.event,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    'Compartilhar',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: whiteColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
